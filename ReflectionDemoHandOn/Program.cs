c871ff7f5dd0bdb1314f076afd44c7b14455945c
sanjay7900 <singhsanjay790043@gmail.com>
2022-06-01 15:45:41 +0530

    initial commit

---
+++ ReflectionDemoHandOn/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemoHandOn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           ReflectionExample example = new ReflectionExample(); 
+            example.ShowInfoOfDllFileAtRunTime();   
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ ReflectionDemoHandOn/ReflectionExample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ReflectionDemoHandOn
+{
+    public class ReflectionExample
+    {
+      
+        public void ShowInfoOfDllFileAtRunTime()
+        {
+          Assembly  assembly = Assembly.LoadFrom(@"D:\aspdotnet\CSharpOPPsRepo\TestDllForTheRelection\bin\Debug\TestDllForTheRelection.dll");
+            Type[] types = assembly.GetTypes();
+            foreach(var t in types)
+            {
+                Console.WriteLine("class name.............");
+                var name=t.Name; 
+                Console.WriteLine(name);  
+                Console.WriteLine(" method name..................");
+                MethodInfo[] mis = t.GetMethods();
+                foreach(MethodInfo mi in mis)
+                {
+                    Console.WriteLine(mi.Name);
+                }
+                Console.WriteLine("Proerty name.....................");
+                PropertyInfo[] properties = t.GetProperties();
+                foreach(PropertyInfo pi in properties)
+                {
+                    Console.WriteLine(pi.Name); 
+                   
+                }
+
+            }
+        }
+
+    }
+}
