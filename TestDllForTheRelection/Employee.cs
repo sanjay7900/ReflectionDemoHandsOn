cc986d14c691d39a2ebc4642235454b533bcc776
sanjay7900 <singhsanjay790043@gmail.com>
2022-06-02 15:47:59 +0530

    initial commit

---
+++ TestDllForTheRelection/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDllForTheRelection
+{
+    public class Employee
+    {
+        public string EmployeeName { get; set; }
+        public string EmployeeDePartment { get; set; }
+        public int EmployeeId { get; set; }
+        public void EmployeeInfo()
+        {
+            Console.WriteLine("EmployeeId: " + EmployeeId + "\nName: " + EmployeeName + "\n" + "EmployeeDepartment: " + EmployeeDePartment);
+
+        }
+
+
+
+
+
+        
+    }
+}
